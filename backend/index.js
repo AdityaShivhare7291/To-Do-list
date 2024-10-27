7bed4665f15b9996e2538720207c9fd681c6f62c
Adityashivhare7291 <adityashivhare7291@gmail.com>
2024-10-27 13:41:58 +0530

    changes are done

--- backend/index.js
+++ backend/index.js
@@ -2,26 +2,30 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const authuser = require('./Routers/authRoutes');
+const taskRoutes = require('./Routers/taskRoutes');
 
 const cors = require('cors');
 
 const { DatabaseLoader } = require('./loaders/DatabaseLoader');
-const { RoutesLoader } = require('./loaders/RoutesLoader');
+//const { RoutesLoader } = require('./loaders/RoutesLoader');
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.use(cors({
-  origin: 'https://to-do-list-seven-sand.vercel.app'
-}));
-
+app.use('/auth', authuser);
+app.use('/tasks', taskRoutes)
 app.get('/', (req, res) => {
   return res.send("hello");
 });
 
+app.use(cors({
+  origin: 'https://to-do-list-seven-sand.vercel.app'
+}));
+
 DatabaseLoader.init(app);
-RoutesLoader.initRoutes(app);
+//RoutesLoader.initRoutes(app);
 
 
 app.listen(PORT, () => {
--- backend/loaders/RoutesLoader.js
+++ backend/loaders/RoutesLoader.js
@@ -1,14 +1,14 @@
-const authuser = require('../Routers/authRoutes');
-const taskRoutes = require('../Routers/taskRoutes');
+// const authuser = require('../Routers/authRoutes');
+// const taskRoutes = require('../Routers/taskRoutes');
 
-class RoutesLoader {
-  static initRoutes(app) {
-    app.use('/auth', authuser);
-    app.use('/tasks', taskRoutes);
-    app.use('/', async (req, res) => {
-      res.status(404).send('No such route found in the API.');
-    });
-  }
-}
+// class RoutesLoader {
+//   static initRoutes(app) {
+//     app.use('/auth', authuser);
+//     app.use('/tasks', taskRoutes);
+//     app.use('/', async (req, res) => {
+//       res.status(404).send('No such route found in the API.');
+//     });
+//   }
+// }
 
-module.exports = { RoutesLoader };
+// module.exports = { RoutesLoader };
