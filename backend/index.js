8086533db27f96a8f700e0accd0aba03d1c8d995
Adityashivhare7291 <adityashivhare7291@gmail.com>
2024-10-27 12:34:33 +0530

    changes are done

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,34 @@
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
-
-
 const server = http.createServer(app);
 
 const { DatabaseLoader } = require('./loaders/DatabaseLoader');
 const { RoutesLoader } = require('./loaders/RoutesLoader');
 
-const bodyParser = require('body-parser');
-
-// Define the port
 const PORT = process.env.PORT || 8080;
 
-// Connect to MongoDB using the URI from .env
+// Define CORS options
+const corsOptions = {
+  origin: 'https://to-do-list-seven-sand.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
 
-
-// const corsOptions = {
-//   origin: 'https://to-do-list-seven-sand.vercel.app',
-//   // Allow this origin
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed HTTP methods
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-// };
+app.use(cors(corsOptions)); // Apply CORS with options
+app.options('*', cors(corsOptions)); // Handle preflight requests
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors());
 
-
-
-
-//app.options('*', cors());
-
+// Load database and routes
 DatabaseLoader.init(app);
-
 RoutesLoader.initRoutes(app);
 
 server.listen(PORT, () => {
