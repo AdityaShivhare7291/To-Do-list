eeca87212c747780c4883548b19db88e57cfa21f
Adityashivhare7291 <adityashivhare7291@gmail.com>
2024-10-27 13:35:01 +0530

    changes are done

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const app = express();
 
 const cors = require('cors');
@@ -11,9 +10,11 @@ const { RoutesLoader } = require('./loaders/RoutesLoader');
 
 const PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(bodyParser.json());
+
+app.use(cors({
+  origin: 'https://to-do-list-seven-sand.vercel.app'
+}));
 
 app.get('/', (req, res) => {
   return res.send("hello");
@@ -21,22 +22,6 @@ app.get('/', (req, res) => {
 
 DatabaseLoader.init(app);
 RoutesLoader.initRoutes(app);
-
-// app.use(cors({
-//   origin: 'https://to-do-list-seven-sand.vercel.app'
-// }));
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://to-do-list-seven-sand.vercel.app');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   if (req.method === 'OPTIONS') {
-//     return res.sendStatus(200);
-//   }
-//   next();
-// });
-
-// Load database and routes
 
 
 app.listen(PORT, () => {
