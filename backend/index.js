ebb4e716354a56ba6891a9313fa2c6bd74f09726
Adityashivhare7291 <adityashivhare7291@gmail.com>
2024-10-27 13:27:22 +0530

    changes are done

--- backend/index.js
+++ backend/index.js
@@ -19,23 +19,25 @@ app.get('/', (req, res) => {
   return res.send("hello");
 });
 
-// app.use(cors({
-//   origin: 'https://to-do-list-seven-sand.vercel.app'
-// }));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://to-do-list-seven-sand.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
-// Load database and routes
 DatabaseLoader.init(app);
 RoutesLoader.initRoutes(app);
+
+app.use(cors({
+  origin: 'https://to-do-list-seven-sand.vercel.app'
+}));
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://to-do-list-seven-sand.vercel.app');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
+
+// Load database and routes
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
